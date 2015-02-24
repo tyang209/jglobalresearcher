c929e760f6284025fbc28f8c14a103729b9580c2
tyang209 <tyang209@gmail.com>
2015-02-24 13:17:17 -0500

    added papers scraping

--- jglobalresearcher/spiders/jglobalresearch_spider.py
+++ jglobalresearcher/spiders/jglobalresearch_spider.py
@@ -72,7 +72,59 @@ class jglobalresearcher_spider(CrawlSpider):
     		except TypeError:
     			
     		return mainDict
+    	def scrapePapers(mainDict,innerHTML):
+    		papers = []
+    		soup = BeautifulSoup(innerHTML)
 
+			#translation of unicode headers into english
+			translationDict = {u'\u30da\u30fc\u30b8':'Page',
+			                   u'\u53f7':'Issue',
+			                    u'\u5dfb':'Volume',
+			                    u'\u8cc7\u6599\u540d':'JournalName',
+			                    u'\u8457\u8005':'Authors',
+			                    u'\u767a\u884c\u5e74':'PubYear'}
+
+			#re pattern for finding JGLOBALIDs
+			jglobal_id_regex = re.compile(r'.*JGLOBAL_ID=(\d+)')
+
+			linkArray=[]
+			#only return divs that are direct decendents.
+			#beautiful soup automatically adds a html,body fields
+
+			for index,div in enumerate(soup.html.body.findAll('div',recursive=False)):
+
+			    paperDict = {}
+			    
+			    link = div.find('a')['href']
+			    
+			    if link in linkArray:
+			        continue
+			        
+			    linkArray.append(link)
+			    paperDict['PaperLink'] = anchor['href']
+			    match = jglobal_id_regex.match(link)
+			    
+			    if match:
+			        paperDict['PaperinJGLOBAL'] = 'Yes'
+			        paperDict['JGLOBALID'] = match.group(1)   
+			        
+			    for p in div.findAll('p'):
+			        if p['class'] in (['light'],['txtR', 'light']):
+			            paperDict['Title'] = p.text
+
+			        elif p['class'] in [['light','mB10']]:
+			            for span in p.findAll('span',{'class':'fwB'}):
+			                translatedKey = translationDict[span.text.replace(u'\uff1a','').strip()]
+			                paperDict[translatedKey]= span.next_sibling.strip()
+			    papers.append(paperDict)
+		   	return papers
+
+    	def is_element_present(self, xpath):
+		    try: 
+		    	self.driver.find_element_by_xpath(xpath)
+		    except NoSuchElementException: 
+		    	return False
+		    return True
 
 		self.driver.get(response.url)
 		self.driver.set_window_size(1920, 1000)
@@ -82,6 +134,11 @@ class jglobalresearcher_spider(CrawlSpider):
 		mainDict = {}
 		mainDict['JGLOBALID'] = returnJGlobalID(response.url)
 		nameXpath = "//h1[contains(@id,'JD_NMRJ')]"
+		papersMoreButtonXP = "//td[contains(@id,'JD_PA')]//img[contains(@src,'/common/images/btn_more.png')]"
+		miscMoreButtonXP = "//td[contains(@id,'JD_AR')]//img[contains(@src,'/common/images/btn_more.png')]"
+		booksMoreButtonXP = "//td[contains(@id,'JD_BKNAM')]//img[contains(@src,'/common/images/btn_more.png')]"
+		
+
 		WebDriverWait(self.driver,60).until(
 				lambda x: len(x.find_element(By.ID,'JD_NMRJ').text)>0
 			)
@@ -90,6 +147,11 @@ class jglobalresearcher_spider(CrawlSpider):
 		jobTitle_div = self.driver.find_element(By.ID,'JD_CS_3')
 		other_affil = self.driver.find_element(By.ID,'JD_CS_4')
 		research_topics = self.driver.find_element(By.ID,'JD_RFD_J')
+
+		if not is_element_present(self, papersMoreButtonXP):
+			xpath = "//td[contains(@id,'JD_PA')]"
+			elem = self.driver.find_element_by_xpath(xpath)
+			elem.get_attribute('innerHTML')
 
 
 		mainDict = parseDepartmentAffil(mainDict, dept_affil_div.get_attribute('innerHTML'))
